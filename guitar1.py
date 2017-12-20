15e25d9f70dd1693c02dd1ff0bbfcd28e9a09f7a
Martin Weigl <martinweigl48@gmail.com>
2017-12-20 22:41:48 +0100

    added experimental guitar input

---
+++ guitar1.py
@@ -0,0 +1,20 @@
+from pyo import *
+from time import sleep
+
+s = Server(sr=44100, nchnls=4, buffersize=512, duplex=1)
+s.setInOutDevice(9)
+s.boot()
+
+s.start()
+tab = NewTable(1)
+rec = TableRec(Input(), tab)
+
+env = HannTable()
+pos = Randi(min=0, max=0.5, freq=[0.25, 0.3], mul=0.2)
+dns = Randi(min=10, max=30, freq=.1)
+pit = Randi(min=0.99, max=1.01, freq=100)
+grn = Granule(tab, env, dens=30, pitch=0.8, pos=0, dur=1, mul=.1).mix(4).out()
+
+while True:
+    sleep(0.5)
+    rec.play()
